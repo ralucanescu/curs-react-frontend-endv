534e1ad9467a0d11a7c4db7bb97f4894f843116d
Raluca N <raluca.nescu@gmail.com>
2020-09-05 15:40:19 +0300

    Tema - 1

--- src/components/bootcamp/bootcamp.js
+++ src/components/bootcamp/bootcamp.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './bootcamp.css';
+import ButtonComponent from '../button/Button';
 
 const BootcampComponent = props => {
-    const {bootcamp: {name, description}} = props;
+    const {bootcamp: {name, description, date}, handleDelete} = props;
+
     return (
         <div className="card-container">
             <p>{name}</p>
             <p>{description}</p>
+            <p>{`${date.toDateString()}`}</p>
+            <ButtonComponent
+                className="delete"
+                buttonTitle="Delete Bootcamp Item"
+                handleClick={handleDelete}
+            />
         </div>
     )
 }
